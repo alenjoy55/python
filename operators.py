90669a862cf8556236b61c49c0b29ce2be0f06f0
alenjoy55 <alen85292@gmail.com>
2024-08-14 09:00:13 +0100

    update

--- operators.py
+++ operators.py
@@ -1,61 +1,109 @@
 #Arithmetic operators
 
-print(20+22)
+# print(20+22)
 
-print(20-4)
+# print(20-4)
 
-print(20/15)
+# print(20/15)
 
-print(20*3)
+# print(20*3)
 
-print('alen'*5)
+# print('alen'*5)
 
-print(20//22)
+# print(20//22)
 
-print(10**5)
+# print(10**5)
 
-print(2%9)
+# print(2%9)
 
 
-#Assignment operators
+# #Assignment operators
 
-a=10
-a+=5
-print(a)
+# a=10
+# a+=5
+# print(a)
 
-a=20
-a-=8
-print(a)
+# a=20
+# a-=8
+# print(a)
 
-a=10
-a*=5
-print(a)
+# a=10
+# a*=5
+# print(a)
 
-a=10
-a/=5
-print(a)
+# a=10
+# a/=5
+# print(a)
 
-a=10
-a%=5
-print(a)
+# a=10
+# a%=5
+# print(a)
 
-a=10
-a//=5
-print(a)
+# a=10
+# a//=5
+# print(a)
 
-a=10
-a**=5
-print(a)
+# a=10
+# a**=5
+# print(a)
 
-#Comparsion operators
-print("comparison")
-print(10==10)
-print(10!=6)
-print(10>9)
-print(10>20)
-print(10<15)
-print(10<3)
-print(10>=9)
-print(10>=20)
-print(10<=20)
-print(10<=3)
+# #Comparsion operators
+# print("comparison")
+# print(10==10)
+# print(10!=6)
+# print(10>9)
+# print(10>20)
+# print(10<15)
+# print(10<3)
+# print(10>=9)
+# print(10>=20)
+# print(10<=20)
+# print(10<=3)
+
+#Logical operators
+
+# print(10==10 and 10>=9)
+# print(10==11 and 10>=9)
+# print(10==10 and 10>=11)
+
+# print(10==11 or 10>=10)
+# print(10==10 or 10>=19)
+# print(10==11 or 10>=11)
+
+# print(not(10==10))
+# print(not(10>=15))
+# print(not(10==9))
+
+#Membership operators 
+# a="alen"
+# l="n"
+# print(l in a)
+# b="len"
+# c="a"
+# print(c in b)
+
+# f="book"
+# g="f"
+# print(g not in f)
+
+#Identity operator
+
+# a=10
+# b=10
+# print(a==b)
+# print(id(a))
+# print(id(b))
+# print (a is b)
+
+# c=10
+# d=12
+# print(c==d)
+# print(id(c))
+# print(id(d))
+# print(c is not d)
+
+a="alen"
+print('a'in "alen" and 'f'not in "alen")
+print(id(a))
+print(len(a))
+
---
+++ string.py
@@ -0,0 +1,19 @@
+#Strings
+
+
+a="python"
+print(type(a))
+b='[1,2,345]'
+print(type(b))
+
+print('pyt\nhon','\tjava')
+
+a="python"
+print("lang = ",a)
+
+print(a[-6])
+print(a[-1])
+
+a="""python
+php"""
+print(a)
