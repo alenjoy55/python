724ba31eed949e379f1448f83a2613b86b862431
alenjoy55 <alen85292@gmail.com>
2024-08-15 09:06:13 +0100

    update

--- string.py
+++ string.py
@@ -1,19 +1,74 @@
 #Strings
 
 
-a="python"
-print(type(a))
-b='[1,2,345]'
-print(type(b))
+# a="python"
+# print(type(a))
+# b='[1,2,345]'
+# print(type(b))
 
-print('pyt\nhon','\tjava')
+# print('pyt\nhon','\tjava')
 
-a="python"
-print("lang = ",a)
+# a="python"
+# print("lang = ",a)
 
-print(a[-6])
-print(a[-1])
+# print(a[-6])
+# print(a[-1])
 
-a="""python
-php"""
-print(a)
+# a="""python
+# php"""
+# print(a)
+
+
+
+# a="alen"
+# print(a[0:3])
+# print(a[2])
+# print(a[:3])
+# print(a[-4])
+# print(a[-5:])
+
+
+#slice with string reverse
+
+
+# a="alenjoy"
+# print(a[::3])
+
+# print(a[::-1])
+# print(a[::4])
+
+
+# a=input("enter a string")
+# print(a[::3])
+# print(a[::-1])
+# print(a==a[::-1])
+
+
+#String methods
+# a="welcome to all"
+# print(a.capitalize())
+# print(a.upper())
+# print(a.lower())
+
+# b="ALEN"
+# print(b.isupper())
+# b="alen"
+# print(b.islower())
+
+# c="python"
+# print(c.center(10),'*')
+# print(c.count('e'))
+# print(c.endswith('n'))
+# print(c.startswith('p'))
+# print(c.find('t'))
+# print(c.index('p'))
+# c='      python        '
+# print(c)
+# print(c.strip())
+# print(c.lstrip())
+# print(c.rstrip())
+# d="456"
+# print(d.isdigit(
+print("{:<10} {:<8} {:<8}".format("name",'age','mark'))
+print("{:<10} {:<8} {:<8}".format("aaa",'20','55'))
+print("{:<10} {:<8} {:<8}".format("bbb",'25','60'))
