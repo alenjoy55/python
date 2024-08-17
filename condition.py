dff053f91d74424a83dd02e38325944bf5aee580
alenjoy55 <alen85292@gmail.com>
2024-08-17 08:57:40 +0100

    update

--- condition.py
+++ condition.py
@@ -39,12 +39,86 @@
 # else:
 #     print("not eligible")
 
-s=int(input("enter number"))
-l=s%10
-if l%3==0:
-    print("possible")
-else:
-    print("not possible")
-    
+# s=int(input("enter number"))
+# l=s%10
+# if l%3==0:
+#     print("possible")
+# else:
+#     print("not possible")
 
+#elif 
+# a=int(input("enter 1st number"))
+# b=int(input("enter a 2nd number"))
+# if a==b:
+#     print("equal")
+# elif a>b:
+#     print(a)
+# else:
+#     print(b)
+    
+# num=int(input("enter a number"))
+# if num==1:
+#     print("sunday")
+# elif num==2:
+#     print("monday")
+# elif num==3:
+#     print("tuesday")
+# elif num==4:
+#     print("wenesday")
+# elif num==5:
+#     print("thursday")
+# elif num==6:
+#     print("friday")
+# elif num==7:
+#     print("saturday")
+# else:
+#     print("not valid")    
+    
+# city=(input("enter a city : "))
+# if city=="delhi":
+#     print("red fort")
+# elif city=="agra":
+#     print("taj mahal")
+# elif city=="jaipur":
+#     print("jal mahal")
+# else:
+#     print("invalid")
+
+# cost=int(input("enter a cost price : "))
+# if cost>=100000:
+#     t=cost*0.15
+#     print(t)
+#     print("tax is 15% =",t) 
+# elif cost>=50000 and cost<=100000:
+#     t=cost*0.10
+#     print("tax is 10%=",t)
+# elif cost<=50000:
+#     t=cost*0.05
+#     print("tax is 5% =",t)
+
+# u=int(input("enter a unit : "))
+# if u<=100:
+#     print("no charge")
+# elif u<=200:
+#     b=(u-100)*5
+#     print(b)
+# else:
+#     b=(u-200)*10+500
+#     print(b)
+
+#nested if
+
+# a=int(input("enter number1 : "))
+# b=int(input("enter number2 : "))
+# c=int(input("enter number3 : "))
+# if a>b:
+#     if a>c:
+#         print(a)
+#     else:
+#         print(c)
+# else:
+#     if b>c:
+#         print(b)
+#     else:
+#         print(c)       
     
