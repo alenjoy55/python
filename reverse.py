49050cf38d27ee53241e44edcbc4cdc483d2fe4b
Alen joy <alen85292@gmail.com>
2024-09-05 13:38:58 +0530

    update

---
+++ reverse.py
@@ -0,0 +1,21 @@
+# a=int(input("enter a number: "))
+# numbers={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+# s=''
+# while a>0:
+#     d=a%10
+#     s=numbers[d]+' '+s
+#     a//=10
+# print(s)
+#output
+'''enter a number: 123 
+one two three '''
+l=[{'name':'alen','age':20,'lang':['mal','eng']}]
+print(l)
+s=input("enter a new language: ")
+l[0]['lang'].append(s)
+print(l)
+
+
+
+
+
