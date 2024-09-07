df61af7f7a915650941b5fb5301012e9d02fede0
alenjoy55 <alen85292@gmail.com>
2024-09-07 13:27:57 +0530

    update

--- set.py
+++ set.py
@@ -14,9 +14,9 @@
 #output
 '''[1, 2, 3, 4, 5]'''
 
-s={1,2,3,4,5}
-s1={1,2,3,4}
-print(s.difference(s1))
+# s={1,2,3,4,5}
+# s1={1,2,3,}
+# print(s.difference(s1))
 # s.add(0)
 # print(s)
 # s.discard(3)
@@ -27,3 +27,52 @@ print(s.difference(s1))
 # print(s)
 # s.update({6,7,8,9})
 # print(s)
+# print(s.intersection(s1))
+# print(s.union(s1))
+# print(s.isdisjoint(s1))
+# print(s.issubset(s1))
+# print(s.issuperset(s1))
+# print(s.symmetric_difference(s1))
+
+# s={1,2,3,4,5}
+# s1={1,2,3,6,7}
+# s.difference_update(s1)
+# print(s)
+# s.intersection_update(s1)
+# print(s)
+# s.symmetric_difference_update(s1)
+# print(s)
+
+#prgm
+#limit
+php=set()
+l=int(input("enter a limit in php: "))
+for i in range (l):
+    name=input("enter a name: ")
+    php.add(name)
+print(php)
+python=set()
+l=int(input("enter a limit in python: "))
+for i in range (l):
+    name=input("enter a name: ")
+    python.add(name)
+print(python)
+java=set()
+l=int(input("enter a limit in java: "))
+for i in range(l):
+ name=input("enter a name: ")
+ java.add(name)
+print(java)
+
+# a=php.intersection(python)
+# a.intersection(java)
+# print("common ",a)
+
+# a=(python.difference(php))
+# a.difference(java)
+# print("python students: ",a)
+
+
+
+
+
