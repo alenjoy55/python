1fcf0768a7bfad2aee3a58f28e5815c4ae0cde36
alenjoy55 <alen85292@gmail.com>
2024-09-12 13:30:22 +0530

    update

---
+++ task/fun.py
@@ -0,0 +1,5 @@
+def sample(name='abc',age=20):
+    print(name,age)
+sample()
+sample('alen')
+sample(age=30)
--- task/wrk.py
+++ task/wrk.py
@@ -4,12 +4,17 @@ def login():
    uname=input("enter uname")
    passw=input("enter passw")
    f=0
+   user=''
    if uname=='alen' and passw=='joy':
       f=1
-      return f
+   for i in emp:
+      if i['id']==uname and i['password']==passw:
+         f=2
+         user=i   
+   return f,user
    
 def add_emp():
-   id=int(input("enter emp id"))
+   id=str(input("enter emp id"))
    f1=0
    for i in emp:
       if i['id']==id:
@@ -22,11 +27,12 @@ def add_emp():
     salary=int(input("enter emp salary"))
     place=input("enter emp place")
     dob=input("date of birth")
-    emp.append({'id':id,'name':name,'age':age,'salary':salary,'place':place,'dob':dob})
+    pasw=dob
+    emp.append({'id':id,'name':name,'age':age,'salary':salary,'place':place,'dob':dob,'password':pasw})
     print(emp)
 
 def update():
-   id=int(input("enter emp id"))
+   id=str(input("enter emp id"))
    f1=0
    for i in emp:
       if i['id']==id:
@@ -44,7 +50,7 @@ def update():
    if f1==0:
       print("invalid id")
 def delete():
-   id=int(input("enter emp id"))
+   id=str(input("enter emp id"))
    f1=0
    for i in emp:
       if i['id']==id:
@@ -56,6 +62,25 @@ def display():
    print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format('id','name','age','salary','place','dob'))
    for i in emp:
             print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format(i['id'], i['name'], i['age'],i['salary'],i['place'],i['dob']))
+def view_profile():
+   print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format('id','name','age','salary','place','dob'))
+   print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format(user['id'], user['name'], user['age'],user['salary'],user['place'],user['dob']))
+def edit_profile():
+   f1=0
+   for i in emp:
+      if i['id']==user['id']:
+         f1=1
+         name=input("enter emp name")
+         age=int(input("enter emp age"))
+         place=input("enter emp place")
+         dob=input("date of birth")
+         i['name']=name
+         i['age']=age
+         i['place']=place
+         i['dob']=dob
+   if f1==0:
+      print("invalid id")
+   
             
          
       
@@ -66,7 +91,7 @@ while True:
     ''') 
    ch=int(input("enter ur choice"))
    if ch==1:
-      f=login()
+      f,user=login()
       if f==1:
         while True:
            print('''
@@ -89,6 +114,25 @@ while True:
               break    
       elif f==0:
         print("invalid uname or passw")
+      elif f==2:
+         while True:
+            if user['dob']==user['password']:
+               passw=input("enter new password")
+               user['password']=passw
+            else:
+               print('''
+                  1.view profile
+                  2.edit profile
+                  3.logout 
+               ''')
+               sub_ch1=int(input("enter ur choice"))
+               if sub_ch1==1:
+                  view_profile()
+               elif sub_ch1==2:
+                  edit_profile()
+               elif sub_ch1==3:
+                  break
+
               
            
 
