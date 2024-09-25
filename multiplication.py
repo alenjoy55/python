54cec3afe9443a4eb9e4c33b30c0b7378b125066
alenjoy55 <alen85292@gmail.com>
2024-09-25 06:35:40 +0530

    update

--- multiplication.py
+++ multiplication.py
@@ -1,9 +1,13 @@
-f=open('python/multi.txt','a')
+f=open('python/multi.txt2','a')
 no=int(input("enter a number: "))
-i=1
-for i in range(1,10+1):
-   c= print(i,'*',no,'=',i*no)
-   f.write(str(i)+'*'+'*'+str(no)+'='+str(i*no)+'\n')
+# i=1
+# for i in range(1,10+1):
+#    c= print(i,'*',no,'=',i*no)
+#    f.write(str(i)+'*'+'*'+str(no)+'='+str(i*no)+'\n')
+for i in range(1,no+1):
+    for j in range(1,11):
+        f.write(str(j)+'*'+str(i)+'='+str(i*j)+'\n')
+        f.write('\n')
    
    
 
