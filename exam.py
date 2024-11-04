4db0a783b79eca792b787bd2481556e2f4c44beb
alenjoy55 <alen85292@gmail.com>
2024-11-04 13:31:00 +0530

    update

---
+++ exam.py
@@ -0,0 +1,17 @@
+# a='orange'
+# l=len(a)
+# i=0
+# rev=''
+# while i<l:
+#     rev=a[i]+rev
+#     i+=1
+# print(rev)
+
+# l=[11,2,3,4,5]
+# l1=max(l)
+# print(l1)
+
+l=['apple','kiwi','orange']
+for i in l:
+    a=len(i)
+    print(i)
